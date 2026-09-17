5e28c967e261bc0b4c32ef158cf83e0735fa5181
Fabio Vinicius Goes Amaral <fabiovini08@hotmail.com>
2026-09-17 16:00:50 -0300

    Update rossler.py

--- src/pipeline/problems/rossler.py
+++ src/pipeline/problems/rossler.py
@@ -43,11 +43,11 @@ class LorenzProblem(Problem):
         true_coeff = np.zeros((n_features, len(self.state_names)))
         a, b, c = self.true_params
         # Feature order: 1, x, y, x^2, xy, y^2, ...
-        true_coeff[3, 0] = -1.0  # dx/dt = -y -z
-        true_coeff[4, 0] = -1.0  
-        true_coeff[2, 1] = 1.0  # dy/dt = x + a*y
-        true_coeff[3, 1] = a
-        true_coeff[1, 2] = b #dz/dy = b + zx - zc
-        true_coeff[8, 2] = 1.0 
-        true_coeff[4, 2] = -c 
+        true_coeff[2, 0] = -1.0  # dx/dt = -y -z
+        true_coeff[3, 0] = -1.0  
+        true_coeff[1, 1] = 1.0  # dy/dt = x + a*y
+        true_coeff[2, 1] = a
+        true_coeff[0, 2] = b #dz/dy = b + zx - zc
+        true_coeff[6, 2] = 1.0 
+        true_coeff[3, 2] = -c 
         return true_coeff
